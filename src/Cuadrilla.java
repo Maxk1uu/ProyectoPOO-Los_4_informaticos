5df360ff2804da61d523f9ba3e3815c040258a02
Maxk1uu <MaxiGTO993@gmail.com>
2025-09-23 01:01:57 -0300

    Clase Cuadrilla creada. Falta revisar.

---
+++ src/Cuadrilla.java
@@ -0,0 +1,67 @@
+//Codigo hecho por: Maximiliano Maureira
+//Revisado por:
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Cuadrilla {
+    //Atributos
+    private int id;
+    private String nombre;
+    private static int maximoCosechadores;
+
+    //Relaciones
+    PlanCosecha planCosecha;
+    ArrayList<CosechadorAsignado> cosechadoresAsignados = new ArrayList<>();
+    Supervisor supervisor;
+
+    //Constructor
+    public Cuadrilla(int id, String nom, Supervisor sup, PlanCosecha plan) {
+        this.id = id;
+        this.nombre = nom;
+        this.planCosecha = plan;
+        this.supervisor = sup;
+    }
+
+    //Metodos
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Supervisor getSupervisor() {
+        return supervisor;
+    }
+
+    public PlanCosecha getPlanCosecha() {
+        return planCosecha;
+    }
+
+    public boolean addCosechador(Date fIni, Date fFin, double meta, Cosechador cos) {
+        CosechadorAsignado cosechador = new CosechadorAsignado(fIni, fFin, meta, nombre,  cos);
+        if (cosechadoresAsignados.contains(cosechador) && cosechadoresAsignados.size() >= maximoCosechadores) {
+            return false; //Si el cosechador ya está asignado o si la cuadrilla ya está llena, retorna falso.
+        }
+
+        cosechadoresAsignados.add(cosechador);
+        return true;
+    }
+
+    public Cosechador[] getCosechadores() {
+        return cosechadoresAsignados.toArray(new Cosechador[0]);
+    }
+
+    public static int getMaximoCosechadores() {
+        return maximoCosechadores;
+    }
+
+    public static void setMaximoCosechadores(int max) {
+        maximoCosechadores = max;
+    }
+}
