c212dab81fb49d5445a36df2084c9bcee73843f2
GabroDegtyarev <gabrielqueson@gmail.com>
2025-09-23 09:55:42 -0300

    PlanCosecha casi completado, AddCosechadorToCuadrilla por terminar.

---
+++ src/EstadoPlan.java
@@ -0,0 +1,7 @@
+
+public enum EstadoPlan {
+    PLANIFICADO,
+    EJECUTANDO,
+    CERRADO,
+    CANCELADO;
+}
---
+++ src/PlanCosecha.java
@@ -0,0 +1,88 @@
+import java.util.Date;
+import java.util.ArrayList;
+public class PlanCosecha {
+
+    private int id;
+    private String nombre;
+    private Date inicio;
+    private Date finEstimado;
+    private Date finReal;
+    private double metaKilos;
+    private double precioBaseKilo;
+    private EstadoPlan estado;
+    // Relacion singular
+    private final Cuartel cuartel;
+    // Relacion multiple
+    private final ArrayList<Cuadrilla> cuadrillas;
+    //Constructor
+    public PlanCosecha(int id, String nombre, Date inicio, Date finEstimado, double metaKilos, double precioBaseKilo, Cuartel cuartel) {
+        this.id = id;
+        this.nombre = nombre;
+        this.inicio = inicio;
+        this.finEstimado = finEstimado;
+        this.metaKilos = metaKilos;
+        this.precioBaseKilo = precioBaseKilo;
+        this.cuartel = cuartel;
+        cuadrillas = new ArrayList<>();
+        estado = EstadoPlan.PLANIFICADO;
+    }
+    //Metodos
+    public int getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Date getInicio() {
+        return inicio;
+    }
+    public Date getFinEstimado() {
+        return finEstimado;
+    }
+    public Date getFinReal() {
+        return finReal;
+    }
+    public void setFinReal(Date finReal) {
+        this.finReal = finReal;
+    }
+    public double getMetaKilos() {
+        return metaKilos;
+    }
+    public void setMetaKilos(double metaKilos) {
+        this.metaKilos = metaKilos;
+    }
+    public double getPrecioBaseKilo() {
+        return precioBaseKilo;
+    }
+    public void setPrecioBaseKilo(double precioBaseKilo) {
+        this.precioBaseKilo = precioBaseKilo;
+    }
+    public EstadoPlan getEstado() {
+        return estado;
+    }
+    public void setEstado(EstadoPlan estado) {
+        this.estado = estado;
+    }
+    public Cuartel getCuartel() {
+        return cuartel;
+    }
+    // Asociacion Multiple de esta clase.
+    public boolean addCuadrilla(int idCuadrilla, String nombreCuadrilla, Supervisor supervisor) {
+        return cuadrillas.add(new Cuadrilla(idCuadrilla, nombreCuadrilla, supervisor, this));
+    }
+    public boolean AddCosechadorToCuadrilla(int idCuadrilla, Date fechaInicio, Date fechaFin, double meta, Cosechador cosechador) {
+
+    }
+    //Retorna los objetos asociados, chequear en el si es que funciona.
+    public Cuadrilla[] getCuadrillas() {
+        return cuadrillas.toArray(new Cuadrilla[0]);
+    }
+
+
+
+
+
+}
