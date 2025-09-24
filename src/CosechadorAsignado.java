8b1c15fcdc3d4bb447f70cda65c90c12d52fb763
Maxk1uu <MaxiGTO993@gmail.com>
2025-09-24 10:17:49 -0300

    Clase CosechadorAsignado creada y lista

---
+++ src/CosechadorAsignado.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+//Codigo hecho por: Maximiliano Maureira
+//Revisado por:
+public class CosechadorAsignado {
+    //Atributos
+    private Date desde;
+    private Date hasta;
+    private double metaKilos;
+
+    //Relaciones
+    Cosechador cosechador;
+    Cuadrilla cuadrilla;
+
+    //Constructor
+    public CosechadorAsignado(Date fIni, Date fFin, double meta, Cuadrilla cuad, Cosechador cos) {
+        this.desde = fIni;
+        this.hasta = fFin;
+        this.metaKilos = metaKilos;
+        this.cuadrilla = cuad;
+        cosechador = cos;
+    }
+
+    //Metodos
+    public Date getDesde() {
+        return desde;
+    }
+
+    public void setDesde(Date desde) {
+        this.desde = desde;
+    }
+
+    public Date getHasta() {
+        return hasta;
+    }
+
+    public void setHasta(Date hasta) {
+        this.hasta = hasta;
+    }
+
+    public double getMetaKilos() {
+        return metaKilos;
+    }
+
+    public void setMetaKilos(double metaKilos) {
+        this.metaKilos = metaKilos;
+    }
+
+    public Cuadrilla getCuadrilla() {
+        return cuadrilla;
+    }
+    public Cosechador getCosechador() {
+        return cosechador;
+    }
+}
