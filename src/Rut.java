30a343e2480e2ce271cfc207f9d987a74d9888f1
Maxk1uu <MaxiGTO993@gmail.com>
2025-09-25 11:38:10 -0300

    Implementé ArrayList para registrar los ruts de cada tipo de Persona

--- src/Rut.java
+++ src/Rut.java
@@ -1,9 +1,16 @@
 //Codigo hecho por: Maximiliano Maureira
 //Revisado por:
+
+import java.util.ArrayList;
 public class Rut {
     //Atributos
     private String rut;
     private String codIdentificador;
+
+    //Registro de ruts por Tipo de Persona
+    public static ArrayList<Rut> rutsPropietarios = new ArrayList<>();
+    public static ArrayList<Rut> rutsSupervisores = new ArrayList<>();
+    public static ArrayList<Rut> rutsCosechadores = new ArrayList<>();
 
     //Constructor
     public Rut(String rut) {
