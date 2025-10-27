15c55f8fd849ae8e5b02e845819c7461ac745c14
rikyquintana <ricardoquintanamendez28@gmail.com>
2025-10-27 10:48:30 -0300

    Cambios en la clase Huerto Implementado: Manejo de excepciones y creacion del metodo setEstadoCuartel

--- src/modelo/Huerto.java
+++ src/modelo/Huerto.java
@@ -1,4 +1,8 @@
-package modelo;// Revisado por: Gabriel Rojas
+// Revisado por: Gabriel Rojas
+package modelo;
+
+import utilidades.EstadoFenologico;
+import utilidades.GestionHuertosException;
 
 import java.util.ArrayList;
 
@@ -11,7 +15,7 @@ public class Huerto {
     // Relacion singular
     private Propietario propietario;
     // Relacion Multiple
-    private final ArrayList <Cuartel> cuarteles;
+    private final ArrayList<Cuartel> cuarteles;
 
     //Constructor
     public Huerto(String nombre, float superficie, String ubicacion, Propietario propietario) {
@@ -58,33 +62,58 @@ public class Huerto {
         this.propietario = (Propietario) propietario;
     }
 
-    public boolean addCuartel(int id, float sup, Cultivo cult){
+    public void addCuartel(int id, float sup, Cultivo cult) throws GestionHuertosException {
         // Verifico si el cuartel pasado como parametro no existe en la lista
-        for(Cuartel c:cuarteles){
-            if(c.getId() == id){ // si es true ya existe
-                return false;
+        for (Cuartel c : cuarteles) {
+            if (c.getId() == id) { // si es true ya existe
+                throw new GestionHuertosException("Ya existe en el huerto un cuartel con id indicado");
             }
         }
-        return cuarteles.add(new Cuartel(id,sup,cult,this));
+        // Verifico que la superficie del cuartel + los cuarteles que tiene el huerto, no supere la superficie del huerto
+        float suma = 0;
+        for (Cuartel c : cuarteles) {
+            suma += c.getSuperficie();
+        }
+        suma += sup;
+        if (suma <= superficie) {
+            cuarteles.add(new Cuartel(id, sup, cult, this));
+        } else {
+            throw new GestionHuertosException("La superficie del cuartel supera el limite del huerto, al sumarle la superficie de sus cuarteles actuales");
+        }
     }
 
-    public Cuartel getCuartel(int id){
+    public Cuartel getCuartel(int id) {
         // Verifico si el cuartel pertence al modelo.Huerto
-        for(Cuartel c:cuarteles){
-            if(c.getId() == id){
+        for (Cuartel c : cuarteles) {
+            if (c.getId() == id) {
                 return c;
             }
         }
         return null; // si no lo encuentra retorna null
     }
 
-    public Cuartel [] getCuartels(){
-        Cuartel [] cuartelesArr = new Cuartel [cuarteles.size()];
+    public Cuartel[] getCuartels() {
+        Cuartel[] cuartelesArr = new Cuartel[cuarteles.size()];
         int cont = 0; // contador para guardar en la posicion correcta
-        for(Cuartel c:cuarteles){
+        for (Cuartel c : cuarteles) {
             cuartelesArr[cont] = c;
             cont++;
         }
         return cuartelesArr;
+    }
+
+    public void setEstadoCuartel(int id, EstadoFenologico estadoFenologico) throws GestionHuertosException {
+        for (Cuartel c : cuarteles) {
+            if (c.getId() == id) {
+                if(c.getEstado() != estadoFenologico) {
+                    // si es distinto podemos cambiar su estado
+                    c.setEstado(estadoFenologico);
+                } else {
+                    throw new GestionHuertosException("No esta permitido el cambio de estado solicitado");
+                }
+            }
+        }
+        // Si no existe el cuartel tiro la excepcion
+        throw new GestionHuertosException("No existe un cuartel con el id indicado");
     }
 }
