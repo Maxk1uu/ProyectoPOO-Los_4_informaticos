9b72c37285dd48d345f1bf54c49cd99347f30cae
Maxk1uu <MaxiGTO993@gmail.com>
2025-11-03 12:33:46 -0300

    Correcion minima

--- src/vista/GestionHuertosApp.java
+++ src/vista/GestionHuertosApp.java
@@ -6,15 +6,11 @@ import utilidades.*;
 import java.util.Scanner;
 
 public class GestionHuertosApp {
-    //Atributos
-    private final Scanner sc = new Scanner(System.in).useDelimiter("[\\t\\n\\r]+"); //Si esto no funciona, hay que agregar el delimitador a cada metodo.
-
     //Relaciones
     private static final GestionHuertosUI gestionHuertosUI = GestionHuertosUI.getInstance();
 
     //Main
     public static void main(String[] args) {
-        GestionHuertosApp app = new GestionHuertosApp();
         gestionHuertosUI.menu();
     }
 }
