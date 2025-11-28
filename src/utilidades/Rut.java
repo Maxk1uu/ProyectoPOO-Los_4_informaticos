7d107263d374e1d2f5153db07ba3ae2f1c367e9b
GabroDegtyarev <gabrielqueson@gmail.com>
2025-11-28 07:43:14 -0300

    Adición de validación de rut en su respectiva clase

--- src/utilidades/Rut.java
+++ src/utilidades/Rut.java
@@ -12,10 +12,18 @@ public class Rut {
         this.numero = numero;
         this.dv = dv;
     }
-    public static Rut of(String rutStr){
+    public static Rut of(String rutStr) throws IllegalArgumentException {
         //Recibira un String con el rut deseado, cortara en la posicion donde esta el '-' y después convertira los dos valores
         //a Long y Char respectivaente, y retornara la creacion de un nuevo objeto rut.
         //En el UML, el constructor rut es private.
+        String formatoValido = "^[0-9]{2}\\.[0-9]{3}\\.[0-9]{3}-[0-9kK]$";
+        //^: Inicia la cadena
+        //[0-9]: Cadena de numeros
+        //{3}: Cadena de 3 digitos
+        //\\. : Indica que hay un punto
+        //[kK]: Indica que hay una k minus o mayus.
+        //$: Finaliza la cadena
+        if (!rutStr.matches(formatoValido)) throw new IllegalArgumentException("El formato del rut dado no es válido");
         String[] cortar = rutStr.split("-");
         //Corta en los puntos.
         String[] rutNum = cortar[0].split("\\.");
